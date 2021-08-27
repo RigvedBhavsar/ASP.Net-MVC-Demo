da93b9f31a12935c91fdca96bb1a8ba3677d07a6
RigvedBhavsar <rigvedbhavsar@gmail.com>
2021-08-27 19:30:04 +0530

    Simple Demo of Asp.Net MVC Application

---
+++ MVC_Demo/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//Acess Model Data into Controller
+using MVC_Demo.Models;
+
+namespace MVC_Demo.Controllers
+{
+    //Intension of this Controller is Enable Communication between Model and View
+    public class ProductsController : Controller
+    {
+        //Created Object of ProdutsData
+        ProductsData db = new ProductsData();
+
+        //Action Method to Crate View of Products List
+        public ViewResult List()
+        {
+            //Passing List to the View
+            return View(db.ProductList.ToList());
+        }
+
+
+        //Action Method to Crate View of Product Details
+        public ViewResult Details(int id)
+        {
+            //Seaching id in products.id using LINQ
+            Product product = db.ProductList.Single(x => x.ProductId == id); 
+            //Passing Product to the view
+            return View(product);
+        }
+    }
+}
---
+++ MVC_Demo/Models/ProductsData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo.Models
+{
+    public class ProductsData
+    {
+        //Creating Property of Name ProductList
+        public IEnumerable<Product> ProductList
+        {
+            get 
+            {
+                List<Product> products = new List<Product>()
+                {
+                    new Product{ ProductId=1 , Name="Samsung TV", Price=34000},
+                    new Product{ ProductId=2 , Name="Apple Watch", Price=21000},
+                    new Product{ ProductId=3 , Name="One Plus 8", Price=45000},
+                    new Product{ ProductId=4 , Name="HP Laptop", Price=55000}
+                };
+                return products;
+            }
+        }
+    }
+}
